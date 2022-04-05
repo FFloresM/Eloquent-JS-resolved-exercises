d13e56954b9b8a2e74400c7a3c5c3b653e5f38bf
Francisco <victoriafrancis2009@gmail.com>
2022-04-05 12:35:38 -0400

    a list completed

--- Chapter 4/aList.js
+++ Chapter 4/aList.js
@@ -15,8 +15,27 @@ function listToArray(list) {
     return arr;
 }
 
+function prepend(element, list) {
+    return {value: element, rest: list};
+}
+
+function nth(list, number) {
+    let pos = 0;
+    while(list.rest != undefined){
+        if(pos == number)
+            break
+        list = list.rest;
+        pos++;
+    }
+    return list.value;
+}
+
 let list = arrayToList([1,2,3]);
 console.log(list);
 let myList = { value: 1, rest: { value: 2, rest: { value: 3, rest: {} } } };
 let arr = listToArray(myList);
 console.log(arr);
+console.log(prepend(4,list)); // puts 4 at begin
+console.log(nth(list,3));
+console.log(nth(list,0)); //why this nth doesn't return 4?
+//YOU have to write a recursive version of nth

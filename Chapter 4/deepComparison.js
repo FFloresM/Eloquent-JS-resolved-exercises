4dd73db8510d9f437350d1979d4c18b21d186b31
Francisco <victoriafrancis2009@gmail.com>
2022-04-13 18:10:11 -0400

    deepEqual

--- Chapter 4/deepComparison.js
+++ Chapter 4/deepComparison.js
@@ -1,9 +1,17 @@
-function deepComparion(value1, value2) {
-    if(typeof value1 === 'object' && value1 !== null){
-        return Object.keys(value1) === Object.keys(value2);
+function deepEqual(value1, value2) {
+    if(typeof value1 == 'object' && value1 != null && typeof value2 == 'object' && value2 != null){
+        for (const key of Object.keys(value2)) {
+            if(!Object.keys(value1).includes(key)){
+                return false;
+            }
+        }
+        return true;
     }else{
         return value1 === value2;
     }
 }
 
-console.log(deepComparion({vaca: "molly"},{vaca: "Tedy"}));
+console.log(deepEqual({vaca: "molly"},{vaca: "Tedy"}));
+console.log(deepEqual(10,10));
+console.log(deepEqual({vaca: "molly"},'hello'));
+console.log(deepEqual({vaca: "molly"},9));

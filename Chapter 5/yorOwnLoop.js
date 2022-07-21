baa8d6c6686addd7243324b03f987abd877c2761
Francisco <victoriafrancis2009@gmail.com>
2022-07-21 19:09:44 -0400

    your own loop

--- Chapter 5/yorOwnLoop.js
+++ Chapter 5/yorOwnLoop.js
@@ -1,3 +1,15 @@
 function loop(value, testF, updateF, bodyF) {
-    
+    while(testF(value)){
+        bodyF(value)
+        value = updateF(value)
+    }
 }
+
+let num = 10;
+const is_positive = function (v) {
+    return v > 0;
+}
+const restar = v => v-1;
+const show_value = v => console.log(v);
+//testing loop function
+loop(num, is_positive, restar, show_value);
